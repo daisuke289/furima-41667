4d7c1497aa755e1c1f59b1cfd3d04c2729568816
daisuke289 <daisuke289@gmail.com>
2025-01-05 08:27:08 +0900

    商品一覧表示機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
-  resources :items, only: [:new, :create, :index] # 必要なアクションを追加
+  resources :items, only: [:index, :show, :new, :create]
 end
