d82abd5cd626fbae9b9b4df04f39083bbcef5f51
daisuke289 <daisuke289@gmail.com>
2025-01-04 17:49:13 +0900

    フォーム実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,25 @@
 class ItemsController < ApplicationController
   def index
-    # ビューの表示だけを行う場合、中身は空でOK
+    # トップページ用の処理（必要に応じて）
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path, notice: '商品が出品されました。'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :category_id, :condition_id, :price, :shipping_fee_id, :prefecture_id,
+                                 :shipping_day_id, :image)
   end
 end
