9861c20131b6e78420c2e3949d86472f42664be3
daisuke289 <daisuke289@gmail.com>
2025-01-05 10:15:25 +0900

    バリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,8 @@
 class Item < ApplicationRecord
+  # ActiveHashのアソシエーション拡張
   extend ActiveHash::Associations::ActiveRecordExtensions
-  def sold_out?
-    purchase.present?
-  end
+
+  # アソシエーション
   belongs_to :user
   belongs_to :category
   belongs_to :condition
@@ -14,12 +14,21 @@ class Item < ApplicationRecord
   has_one :purchase
 
   # バリデーション
+  # 必須項目
   validates :name, presence: true
   validates :description, presence: true
-  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :condition_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :shipping_fee_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :shipping_day_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  validates :price, presence: true,
+                    numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+
+  # ActiveHashを使用した項目のバリデーション
+  validates :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :shipping_day_id,
+            numericality: { other_than: 1, message: "can't be blank" }
+
+  # 画像のバリデーション
+  validates :image, presence: true
+
+  # インスタンスメソッド
+  def sold_out?
+    purchase.present?
+  end
 end
