e4880569b21a34a84f0a92d6af744563bb6454a9
daisuke289 <daisuke289@gmail.com>
2025-01-05 13:44:58 +0900

    javascriptの変更

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,24 +1,21 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const priceInput = document.getElementById("item-price");
-  const addTaxPrice = document.getElementById("add-tax-price");
-
-  const profit = document.getElementById("profit");
-
-  if (priceInput) {
+document.addEventListener('turbo:load', () => {
+    const priceInput = document.getElementById("item-price");
+    const addTaxPrice = document.getElementById("add-tax-price");
+    const profit = document.getElementById("profit");
+  
+    if (priceInput) {
       priceInput.addEventListener("input", () => {
-          const inputValue = parseInt(priceInput.value);
-
-          if (isNaN(inputValue) || inputValue <= 0) {
-              addTaxPrice.innerHTML = 0;
-              profit.innerHTML = 0;
-              return;
-          }
-
-          const tax = Math.floor(inputValue * 0.1); // 販売手数料を計算
-          addTaxPrice.innerHTML = tax;
-
-          const profitValue = inputValue - tax; // 販売利益を計算
-          profit.innerHTML = profitValue;
+        const inputValue = parseInt(priceInput.value, 10);
+        if (isNaN(inputValue) || inputValue <= 0) {
+          addTaxPrice.innerHTML = 0;
+          profit.innerHTML = 0;
+          return;
+        }
+        const tax = Math.floor(inputValue * 0.1);
+        const profitValue = inputValue - tax;
+        addTaxPrice.innerHTML = tax;
+        profit.innerHTML = profitValue;
       });
-  }
-});
+    }
+  });
+  
