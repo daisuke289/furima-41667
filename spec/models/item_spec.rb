a4cdd2a7a166e375eb063754d383ac3002da4117
daisuke289 <daisuke289@gmail.com>
2025-01-13 09:53:41 +0900

    テストコード追加

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -5,63 +5,53 @@ RSpec.describe Item, type: :model do
     @item = FactoryBot.build(:item)
   end
 
-  describe '商品出品機能' do
-    context '出品できる場合' do
-      it 'すべての項目が正しく入力されていれば保存できる' do
-        expect(@item).to be_valid
-      end
+  context '商品の出品ができる場合' do
+    it '全ての項目が正しく入力されていれば保存できる' do
+      expect(@item).to be_valid
+    end
+  end
+
+  context '商品の出品ができない場合' do
+    it 'category_idが空では保存できないこと' do
+      @item.category_id = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Category can't be blank")
     end
 
-    context '出品できない場合' do
-      it '価格が300未満では保存できない' do
-        @item.price = 299
-        @item.valid?
-        expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
-      end
+    it 'condition_idが空では保存できないこと' do
+      @item.condition_id = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Condition can't be blank")
+    end
 
-      it '価格が10,000,000以上では保存できない' do
-        @item.price = 10_000_000
-        @item.valid?
-        expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
-      end
+    it 'shipping_fee_idが空では保存できないこと' do
+      @item.shipping_fee_id = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Shipping fee can't be blank")
+    end
 
-      it '価格が半角数字でなければ保存できない（小数）' do
-        @item.price = 300.5
-        @item.valid?
-        expect(@item.errors.full_messages).to include('Price must be an integer')
-      end
+    it 'prefecture_idが空では保存できないこと' do
+      @item.prefecture_id = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Prefecture can't be blank")
+    end
 
-      it '価格が半角数字でなければ保存できない（文字列）' do
-        @item.price = '三百'
-        @item.valid?
-        expect(@item.errors.full_messages).to include('Price is not a number')
-      end
+    it 'shipping_day_idが空では保存できないこと' do
+      @item.shipping_day_id = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Shipping day can't be blank")
+    end
 
-      it '名前が空では保存できない' do
-        @item.name = ''
-        @item.valid?
-        expect(@item.errors.full_messages).to include("Name can't be blank")
-      end
+    it '価格が空では保存できないこと' do
+      @item.price = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price can't be blank")
+    end
 
-      it '説明が空では保存できない' do
-        @item.description = ''
-        @item.valid?
-        expect(@item.errors.full_messages).to include("Description can't be blank")
-      end
-
-      it '画像が添付されていない場合は保存できない' do
-        @item.image = nil
-        @item.valid?
-        expect(@item.errors.full_messages).to include("Image can't be blank")
-      end
-
-      it 'カテゴリーが未選択（1）の場合は保存できない' do
-        @item.category_id = 1
-        @item.valid?
-        expect(@item.errors.full_messages).to include("Category can't be blank")
-      end
-
-      # 他のActiveHash項目（condition_idなど）も同様にテストを追加
+    it 'ユーザーが紐づいていないと保存できないこと' do
+      @item.user = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include('User must exist')
     end
   end
 end
