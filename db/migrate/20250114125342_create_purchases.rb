5d314ff1be533b9726f1c2d556977323f9613882
daisuke289 <daisuke289@gmail.com>
2025-01-15 19:33:56 +0900

    モデル作成、マイグレード

--- db/migrate/20250114125342_create_purchases.rb
+++ db/migrate/20250114125342_create_purchases.rb
@@ -1,6 +1,7 @@
-class CreatePurchases < ActiveRecord::Migration[7.0]
+class CreatePurchases < ActiveRecord::Migration[6.0]
   def change
     create_table :purchases do |t|
+      t.references :item, null: false, foreign_key: true # 外部キーを追加
 
       t.timestamps
     end
