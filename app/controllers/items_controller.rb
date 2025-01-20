a0142d0fb61d652b2130a0af1a05975cbca58790
daisuke289 <daisuke289@gmail.com>
2025-01-20 14:05:22 +0900

    コントローラーの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
-
+  before_action :authenticate_user!, only: [:new, :create, :edit]
+  before_action :set_item, only: [:show, :edit]
   def index
     @items = Item.order(created_at: :desc)
   end
@@ -22,10 +22,18 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    redirect_to root_path unless current_user == @item.user
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:name, :description, :category_id, :condition_id, :price, :shipping_fee_id, :prefecture_id,
                                  :shipping_day_id, :image)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
