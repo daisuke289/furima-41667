ede25a93a880af4456f66e45470743628a8c5c60
daisuke289 <daisuke289@gmail.com>
2025-01-25 08:19:05 +0900

    createアクションの実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,6 +5,31 @@ class OrdersController < ApplicationController
   end
 
   def create
-    # 購入処理を実装予定
+    @item = Item.find(params[:item_id])
+    @order_form = OrderForm.new(order_form_params)
+    if @order_form.valid?
+      pay_item # クレジットカード決済処理
+      @order_form.save
+      redirect_to root_path
+    else
+      render :index
+    end
   end
+end
+
+private
+
+def order_form_params
+  params.require(:order_form).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number, :token).merge(
+    user_id: current_user.id, item_id: params[:item_id]
+  )
+end
+
+def pay_item
+  Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+  Payjp::Charge.create(
+    amount: @item.price,
+    card: order_form_params[:token],
+    currency: 'jpy'
+  )
 end
