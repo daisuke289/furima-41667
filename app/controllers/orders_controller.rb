075a2e57e75b78720a580454aff15ee0284dd9ad
daisuke289 <daisuke289@gmail.com>
2025-01-25 18:46:40 +0900

    コントローラーの変更

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,28 +8,34 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
     @order_form = OrderForm.new(order_form_params)
     if @order_form.valid?
-      pay_item # クレジットカード決済処理
-      @order_form.save
-      redirect_to root_path
+      begin
+        pay_item # クレジットカード決済処理
+        @order_form.save
+        redirect_to root_path
+      rescue Payjp::PayjpError => e
+        Rails.logger.error("PAY.JPエラー: #{e.message}")
+        flash.now[:alert] = "クレジットカード決済に失敗しました: #{e.message}"
+        render :index
+      end
     else
       render :index
     end
   end
-end
 
-private
+  private
 
-def order_form_params
-  params.require(:order_form).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number, :token).merge(
-    user_id: current_user.id, item_id: params[:item_id]
-  )
-end
+  def order_form_params
+    params.require(:order_form).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number, :token).merge(
+      user_id: current_user.id, item_id: params[:item_id]
+    )
+  end
 
-def pay_item
-  Payjp.api_key = ENV['PAYJP_SECRET_KEY']
-  Payjp::Charge.create(
-    amount: @item.price,
-    card: order_form_params[:token],
-    currency: 'jpy'
-  )
+  def pay_item
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_form_params[:token],
+      currency: 'jpy'
+    )
+  end
 end
