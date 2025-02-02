9f9f4a9b2bb627bf98548fed2f030dd3db5ad512
daisuke289 <daisuke289@gmail.com>
2025-02-02 11:05:20 +0900

    viewを戻す

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -4,18 +4,23 @@ class OrderForm
 
   with_options presence: true do
     validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/, message: 'はハイフンを含めて入力してください' }
-    validates :prefecture_id, numericality: { other_than: 0, message: 'を選択してください' }
+    validates :prefecture_id, numericality: { other_than: 1, message: 'を選択してください' } # ← 変更の可能性
     validates :city
     validates :address
     validates :phone_number, format: { with: /\A0\d{9,10}\z/, message: 'は10桁または11桁の半角数字で入力してください' }
-    validates :token
+    validates :token # ← クレジット決済を考慮し、トークン必須
   end
 
   def save
     purchase = Purchase.create(user_id: user_id, item_id: item_id)
-    ShippingAddress.create(
-      postal_code: postal_code, prefecture_id: prefecture_id, city: city, address: address,
-      building_name: building_name, phone_number: phone_number, purchase_id: purchase.id
-    )
+
+    if purchase.persisted? # Purchaseが保存されたか確認
+      ShippingAddress.create(
+        postal_code: postal_code, prefecture_id: prefecture_id, city: city, address: address,
+        building_name: building_name, phone_number: phone_number, purchase_id: purchase.id
+      )
+    else
+      false # Purchaseが保存されなかった場合は false を返す
+    end
   end
 end
