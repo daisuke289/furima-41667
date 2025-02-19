8ebf8898989b68c2ccbd3baeaab8f9d49398b6bc
daisuke289 <daisuke289@gmail.com>
2025-02-19 21:03:59 +0900

    Update orders_controller.rb

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,12 +1,15 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_item
+  before_action :prevent_self_purchase
+  before_action :prevent_sold_item
+
   def index
-    @item = Item.find(params[:item_id])
     @order_form = OrderForm.new
     gon.public_key = ENV['PAYJP_PUBLIC_KEY']
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @order_form = OrderForm.new(order_params)
     gon.public_key = ENV['PAYJP_PUBLIC_KEY']
     if @order_form.valid?
@@ -26,6 +29,22 @@ class OrdersController < ApplicationController
   end
 
   private
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def prevent_sold_item
+    return unless @item.purchase.present?
+
+    redirect_to root_path
+  end
+
+  def prevent_self_purchase
+    return unless current_user.id == @item.user_id
+
+    redirect_to root_path
+  end
 
   def order_params
     params.require(:order_form).permit(:postal_code, :prefecture_id, :city, :address, :building_name, :phone_number, :token)
