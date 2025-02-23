50dd71b482d9fdac758678ab359d8c30aff7868b
daisuke289 <daisuke289@gmail.com>
2025-02-23 14:19:09 +0900

    tokenをmargeに記載

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -45,8 +45,8 @@ class OrdersController < ApplicationController
   end
 
   def order_params
-    params.require(:order_form).permit(:postal_code, :prefecture_id, :city, :address, :building_name, :phone_number, :token)
-          .merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:order_form).permit(:postal_code, :prefecture_id, :city, :address, :building_name, :phone_number)
+          .merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
   def pay_item
