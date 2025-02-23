17493e14c20a91ac605b03971ea8b22d52df94b0
daisuke289 <daisuke289@gmail.com>
2025-02-23 10:33:47 +0900

    FactoryBot.createに変更

--- spec/models/order_form_spec.rb
+++ spec/models/order_form_spec.rb
@@ -2,12 +2,8 @@ require 'rails_helper'
 
 RSpec.describe OrderForm, type: :model do
   before do
-    @user = FactoryBot.build(:user)
-    @user.id = 1  # 明示的にIDを設定
-
-    @item = FactoryBot.build(:item)
-    @item.id = 1  # 明示的にIDを設定
-
+    @user = FactoryBot.create(:user)
+    @item = FactoryBot.create(:item, user: @user)
     @order_form = FactoryBot.build(:order_form, user_id: @user.id, item_id: @item.id)
   end
 
